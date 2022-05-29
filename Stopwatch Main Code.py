a31a61fd19f781dc0a22e6224e207105f1d8c793
JohnNicole2001 <105397157+JohnNicole2001@users.noreply.github.com>
2022-05-29 22:51:45 +0800

    Update Stopwatch Main Code.py

--- Stopwatch Main Code.py
+++ Stopwatch Main Code.py
@@ -325,42 +325,30 @@ file_menu.add_command(label= "Soundtrack 4", command= soundtrack_4)
 file_menu.add_command(label= "Soundtrack 5", command= soundtrack_5)
 
 #About Us
+#About Us
 from tkinter import *
 root = Tk()
-root.minsize(height = 50, width = 900)
+root.minsize(height = 50, width = 100)
 root.resizable(0,0)
 def tab1():
 	pass
-menubar = tk.Menu(my_w)
-menu_aboutus=tk.Menu(menubar, tearoff=0, bg='Blue')
-menubar.addcascade(label="About Us", menu = menu_aboutus)
-menu_aboutus.add_command(label = "About Us", command = tab1())
     def tab2():
-        label1.destroy()
-        button1.destroy()
-        label2 = Label(root, text = "We are a group of 1st Year College students taking up the degree:\n"
+	my_w_tab2=Toplevel(my_w)
+	my_w_tab2.geometry("200x200")
+	my_w_tab2.title("About Us")
+	label=tk.Label(my_w_tab2, textvariable=my_str1)
+	label.grid(row=2, column=2,padx=10,pady=30)
+	my_str.set("We are a group of 1st Year College students taking up the degree:\n"
         "Bachelor of Science in Electronics and Communications Engineering.\n"
         "Objective: This repository was created for the sole purpose of completing the final project of our Object-oriented Programming course.\n" 
-        "We were tasked to create a Python Stopwatch with a GUI using a library of our choice.",
-        font = ('Times_New_Roman',15))
-        label2.pack()
-        def back():
-            label2.destroy()
-            button2.destroy()
-            tab1()
-        button2 = Button(root, text = 'Back', font = ('Times_New_Roman', 15), command = back)
-        button2.pack(side = BOTTOM)
-    label1 = Label(root, text = "Welcome to the Moontong-Underpaid-Devs:\n"
+        "We were tasked to create a Python Stopwatch with a GUI using a library of our choice." 
+	"Welcome to the Moontong-Underpaid-Devs:\n"
         " Joshua Padayao,\n"
         "Anne Cornelia,\n" 
         "Rizza Claire Mollaneda,\n"
         "Sharry Celines Marcos,\n"
-        "John Nicole Losaria\n", font = ('Times_New_Roman', 15))
-    label1.pack()
-    button1 = Button(root, text = 'Next', font = ('Times_New_Roman', 15), command = tab2)
-    button1.pack(side = BOTTOM)
-button = Button(root, text = 'About Us', font = ('Times_New_Roman', 15), command = tab1)
-button.pack(side = BOTTOM)
-
+        "John Nicole Losaria\n",
+        font = ('Times_New_Roman',10))
+       
 
 root.mainloop()
